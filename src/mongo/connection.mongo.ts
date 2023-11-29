3eae89c676d09dce02440240b045c550e611e30f
pakeku <elina13@wgu.edu>
2023-11-28 23:11:27 -0500

    chore: refactor test and mongo functions

--- src/mongo/connection.mongo.ts
+++ src/mongo/connection.mongo.ts
@@ -1,8 +1,22 @@
 import { MongoClient } from 'mongodb';
 
-export const connectToDatabase =
-    async (): Promise<MongoClient> => await new MongoClient(process.env.MONGO_URL as string);
+export const connectToDatabase = async (): Promise<MongoClient> => {
+    // Check if the MONGO_URL is defined
+    if (!process.env.MONGO_URL) {
+        throw new Error("MONGO_URL environment variable is not defined.");
+    }
 
-export const disconnectFromDatabase = async (client: MongoClient): Promise<void> =>
-    await client.close();
+    // Create a new MongoClient instance
+    const client = new MongoClient(process.env.MONGO_URL);
 
+    // Connect to the database and return the client
+    await client.connect();
+    return client;
+};
+
+export const disconnectFromDatabase = async (client: MongoClient | null): Promise<void> => {
+    // Only attempt to close the connection if the client is not null
+    if (client) {
+        await client.close();
+    }
+};
--- tests/mongo.test.ts
+++ tests/mongo.test.ts
@@ -5,18 +5,30 @@ import { config } from 'dotenv';
 describe('MongoDB Connection Tests', () => {
     beforeAll(() => {
         config();
-    })
+    });
 
-    let connection: MongoClient;
+    let connection: MongoClient | null = null;
 
     test('connect should return a connection object', async () => {
         connection = await connectToDatabase();
-        expect(connection).toBeInstanceOf(MongoClient)
-        expect(connection).not.toBeNull();
+        expect(connection).toBeInstanceOf(MongoClient);
+        // Test if the connection is active by attempting a database operation
+        await expect(connection.db('test').command({ ping: 1 })).resolves.toBeTruthy();
     });
 
     test('disconnect should drop the connection', async () => {
         await expect(disconnectFromDatabase(connection)).resolves.not.toThrow();
-        expect(connection).toBeNull();
+        // After disconnecting, attempting a database operation should fail
+        if (connection) {
+            await expect(connection.db('test').command({ ping: 1 })).rejects.toThrow();
+        } else {
+            fail('Connection is null'); // Fail test if connection is null
+        }
+    });
+
+    afterAll(async () => {
+        if (connection) {
+            await disconnectFromDatabase(connection);
+        }
     });
 });
